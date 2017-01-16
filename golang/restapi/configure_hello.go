32d6bbe7a438d3e809cc87988db3d419358674f7
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-01-16 10:19:32 +0300

    GoLang: Болванка сервиса

---
+++ golang/restapi/configure_hello.go
@@ -0,0 +1,80 @@
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	errors "github.com/go-openapi/errors"
+	runtime "github.com/go-openapi/runtime"
+	middleware "github.com/go-openapi/runtime/middleware"
+	graceful "github.com/tylerb/graceful"
+
+	"github.com/bozaro/tech-db-hello/golang/restapi/operations"
+)
+
+// This file is safe to edit. Once it exists it will not be overwritten
+
+//go:generate swagger generate server --target .. --name hello --spec ../../common/swagger.yml
+
+func configureFlags(api *operations.HelloAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.HelloAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// s.api.Logger = log.Printf
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	api.AddMultiHandler = operations.AddMultiHandlerFunc(func(params operations.AddMultiParams) middleware.Responder {
+		return middleware.NotImplemented("operation .AddMulti has not yet been implemented")
+	})
+	api.DestroyOneHandler = operations.DestroyOneHandlerFunc(func(params operations.DestroyOneParams) middleware.Responder {
+		return middleware.NotImplemented("operation .DestroyOne has not yet been implemented")
+	})
+	api.FindHandler = operations.FindHandlerFunc(func(params operations.FindParams) middleware.Responder {
+		return middleware.NotImplemented("operation .Find has not yet been implemented")
+	})
+	api.GetOneHandler = operations.GetOneHandlerFunc(func(params operations.GetOneParams) middleware.Responder {
+		return middleware.NotImplemented("operation .GetOne has not yet been implemented")
+	})
+	api.UpdateOneHandler = operations.UpdateOneHandlerFunc(func(params operations.UpdateOneParams) middleware.Responder {
+		return middleware.NotImplemented("operation .UpdateOne has not yet been implemented")
+	})
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix"
+func configureServer(s *graceful.Server, scheme string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
