057221ef3c0856bf4daf8a70ac2199b291872f3a
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-01-20 14:54:58 +0300

    Добавил болванку для сервера

---
+++ java-spring/src/main/java/ru/bozaro/helloDb/HelloApplication.java
@@ -0,0 +1,11 @@
+package ru.bozaro.helloDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
