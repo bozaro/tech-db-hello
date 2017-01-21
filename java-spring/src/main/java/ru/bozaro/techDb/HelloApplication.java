15419681c1f61289eab785f9ca40486b99789044
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-01-21 22:21:25 +0300

    Заработал пример для Java

--- java-spring/src/main/java/ru/bozaro/techDb/HelloApplication.java
+++ java-spring/src/main/java/ru/bozaro/techDb/HelloApplication.java
@@ -1,9 +1,15 @@
-package ru.bozaro.helloDb;
+package ru.bozaro.techDb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan
+@EnableAutoConfiguration
 public class HelloApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
