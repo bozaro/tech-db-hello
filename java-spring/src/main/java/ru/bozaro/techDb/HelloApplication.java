dc362cd53ce1fb239620c445896c0f4ebf19489d
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-01-21 22:39:07 +0300

    Скомпилировался

--- java-spring/src/main/java/ru/bozaro/techDb/HelloApplication.java
+++ java-spring/src/main/java/ru/bozaro/techDb/HelloApplication.java
@@ -1,15 +1,11 @@
 package ru.bozaro.techDb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan
-@EnableAutoConfiguration
 public class HelloApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
