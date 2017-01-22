f2a9a9a18b6902ff5d1f9c436cf467758e6e719b
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-01-22 23:29:16 +0300

    Убрал лишний код

--- java-spring/src/main/java/ru/bozaro/techDb/service/SwaggerUIRedirect.java
+++ java-spring/src/main/java/ru/bozaro/techDb/service/SwaggerUIRedirect.java
@@ -1,15 +1,8 @@
 package ru.bozaro.techDb.service;
 
+import io.swagger.configuration.HomeController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class SwaggerUIRedirect {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String redirect(HttpServletRequest request) {
-        return "redirect:swagger-ui.html";
-    }
+public class SwaggerUIRedirect extends HomeController {
 }
